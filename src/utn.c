d393f2db4d7415415c4d70957c7f2691be17b6bc
Ericka Diaz <erickaldiaz@gmail.com>
2020-09-06 03:59:46 -0300

    incomplete

--- src/utn.c
+++ src/utn.c
@@ -4,5 +4,40 @@
  *  Created on: Sep 5, 2020
  *      Author: ericka.diaz
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+int utn_getNumero(int* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo, int reintentos){
+
+	int bufferInt;
+	int retorno = -1;
+	int contadorIntentos = 0;
+
+	if(pResultado != NULL && mensaje != NULL && mensajeError !=NULL && minimo <= maximo && reintentos >= 0)
+	{
+		do{
+			printf("%s", mensaje);
+				scanf("%d", &bufferInt);
+				if(bufferInt >= minimo && bufferInt <= maximo){
+					*pResultado = bufferInt;
+					retorno = 0;
+					break;
+				}else{
+					printf("%s", mensajeError);
+					reintentos--;
+				}
+
+		}while(reintentos > 0);
+	}
+	return retorno;
+}
 
 
+int utn_getCaracter(char* pResultado, char* mensaje, char* mensajeError, char minimo, char maximo, int reintentos){
+
+	int retorno = -1;
+	char bufferChar;
+	if(pResultado != NULL && mensaje)
+
+	return retorno;
+}
